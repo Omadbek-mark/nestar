58468bb1d071dc73ee39b77e11d833a507cb1174
omadbek-mark <omadbekanvarbekov00@gmail.com>
2026-08-20 05:10:49 +0900

    feat: create checkAuthRoles for testing

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -6,6 +6,9 @@ import { Member } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'bson';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
 export class MemberResolver {
@@ -13,14 +16,14 @@ export class MemberResolver {
   
   @Mutation(() => Member)
   public async signup(@Args("input") input: MemberInput): Promise<Member> {
-      console.log('Mutation: signup');
-      return this.memberService.signup(input);
+    console.log('Mutation: signup');
+    return this.memberService.signup(input);
   }
 
   @Mutation(() => Member)
   public async login(@Args("input") input: LoginInput): Promise<Member> {
-      console.log('Mutation: login');
-      return this.memberService.login(input);
+    console.log('Mutation: login');
+    return this.memberService.login(input);
   }
 
   @UseGuards(AuthGuard)
@@ -32,12 +35,20 @@ export class MemberResolver {
     return this.memberService.updateMember();
   }
 
-   @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard)
   @Query(() => String)
   public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
     console.log('Query: checkAuth');
     console.log('memberNick:', memberNick);
     return `Hi ${memberNick}`;
+  }
+
+  @Roles(MemberType.USER, MemberType.AGENT)
+  @UseGuards(RolesGuard)
+  @Query(() => String)
+  public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+    console.log('Query: checkAuthRoles');
+    return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
   }
 
   @Query(() => String)
@@ -48,9 +59,11 @@ export class MemberResolver {
     
   /** ADMIN **/
     
-  //Authorization: ADMIN  
+  @Roles(MemberType.ADMIN)
+  @UseGuards(RolesGuard)
   @Mutation(() => String)
-  public async getAllMembersByAdmin(): Promise<string> {
+  public async getAllMembersByAdmin(@AuthMember() authMember: Member): Promise<string> {
+    console.log("authMember.memberType:", authMember.memberType)
     return this.memberService.getAllMembersByAdmin();
   }
   
